74b1784f94e4d01ae829e520ccf537670fd766db
evangrinalds <egrinalds@gmail.com>
2020-08-14 11:41:17 -0700

    Update the file

--- module4-acid-and-database-scalability-tradeoffs/titanic_queries.py
+++ module4-acid-and-database-scalability-tradeoffs/titanic_queries.py
@@ -1,69 +1,83 @@
 import psycopg2
+import sqlite3
+
+conn = sqlite3.connect('titanic.sqlite3')
+curs = conn.cursor()
+
+
+def execute_query(cursor, query):
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
 '''
-# ## Titanic Data from PostgregDB
-# ---
+# Titanic Data from PostgreDB
+
 # 1- How many passengers survived, and how many died?
 # 2- How many passengers were in each class?
 # 3- How many passengers survived/died within each class?
 # 4- What was the average age of survivors vs nonsurvivors?
 # 5- What was the average age of each passenger class?
 # 6- What was the average fare by passenger class? By survival?
-# 7- How many siblings/spouses aboard on average, by passenger class?
-#  By survival?
-# 8- How many parents/children aboard on average, by passenger class?
-#  By survival?
-# - Do any passengers have the same name?
-# - (Bonus! Hard, may require pulling and processing with Python)
-#  How many married?
-#   couples were aboard the Titanic? Assume that two people (one `Mr.` and one
-#   `Mrs.`) with the same last name
-# and with at least 1 sibling/spouse aboard are
-#   a married couple.
+# 7- How many siblings/spouses aboard on average, by passenger class? By survival?
+# 8- How many parents/children aboard on average, by passenger class? By survival?
+# 8.5 How many married couples were aboard the Titanic?
+
 '''
+# Question 1 How many passengers survived, and how many died?
 passenger_survived = '''
 SELECT count(Survived) as passsenger_survived
 From Titanic as c WHERE Survived = 1;'''
 passenger_died = '''
 SELECT count(Survived) as Died  From Titanic as c WHERE Survived = 0;'''
+
 # Question 2 How many passengers were in each class?
 passenger_count_class = '''
 SELECT pclass, count(Survived) as People_died
 From Titanic as c WHERE Survived = 0 GROUP by Pclass;'''
-# Question 3
+
+# Question 3 How many passengers survived/died within each class?
 passenger_sur_class = '''
 SELECT pclass, Survived ,count(*) as Passenger_count From Titanic as c
 GROUP by Pclass , Survived;'''
+
 # Question 4 What was the average age of survivors vs nonsurvivors?
 Q4 = '''
 SELECT Survived, round(avg(AGE),2) as Average_age From Titanic
 as c GROUP by  Survived;'''
+
 # Question 5 What was the average age of each passenger class?
 Q5 = '''
 SELECT Pclass, round(avg(AGE),2) as Average_age
 From Titanic as c GROUP by  Pclass;'''
+
 # Question 6 What was the average fare by passenger class?
 Q6 = '''
 SELECT Pclass, round(avg(Fare),2), Survived as Avg_fare
 From Titanic as c GROUP by  Pclass, Survived;'''
+
 # Question 6.5 By survival?
 Q6_5 = '''
 SELECT Pclass, round(avg(Fare),2) as AVG_fare, Survived
 From Titanic as c GROUP by  Pclass, Survived;'''
+
 # Question 7  How many siblings/spouses aboard on average,
 # by passenger class? By survival?
 Q7 = '''
 SELECT Pclass, round(avg("Siblings/Spouses Aboard"),2) as Sib_N_Spouse
 from titanic GROUP by Pclass;'''
+
 # Question 7.5 By survival?
 Q7_5 = '''
 SELECT Survived, round(avg("Siblings/Spouses Aboard"),2) as AVG_Sib_N_Spouse
 from titanic2 GROUP by Survived;'''
-# Question 8 How many parents/children aboard on average,
-# by passenger class? By survival?
+
+# Question 8 How many parents/children aboard on average, by passenger class? By survival?
 Q8 = '''
 SELECT Pclass, round(avg("Parents/Children Aboard"),2)
 as Average_Parent_Children
 from titanic2 group by Pclass;'''
+
 # Question 8.5 By survival?
 Q8_5 = '''
 SELECT Survived, round(avg("Parents/Children Aboard"),2)
@@ -78,7 +92,6 @@ pg_conn = psycopg2.connect(dbname=dbname, user=user,
                            password=password, host=host)
 pg_curs = pg_conn.cursor()
 
-
 Q1 = '''
 SELECT count(survived) as passsenger_survived
 From Titanic WHERE survived = 1;'''
@@ -90,10 +103,8 @@ SELECT COUNT(survived) as passsenger_survived
 FROM Titanic WHERE survived = 1;"""
 result = pg_curs.execute(Q1)
 
-
 print('Q1 How many passenger survived?:')
 print('Passenger Survived', pg_curs.fetchall()[0][0], '.')
-
 
 print('Q1_5 How many passenger died?:')
 passenger_died = '''
@@ -114,7 +125,7 @@ for people in test:
 print('Q2 How many passengers were in each class?:')
 test = 'SELECT Pclass, count(*) from titanic GROUP by Pclass;'
 pg_curs.execute(test)
-test1 = pg_curs.fetchall()
+test = pg_curs.fetchall()
 for people in test1:
     print(f'Pclass: {people[0]}, # of people: {people[1]}.')
 
@@ -148,13 +159,12 @@ test = pg_curs.fetchall()
 for people in test:
     print(f'Pclass: {people[0]}, Average Age: {people[1]}.')
 
-
 print("Question 6 What was the average fare by passenger class?")
 Q6 = 'SELECT Pclass, avg(fare) as average_fare From Titanic  GROUP by  Pclass;'
 pg_curs.execute(Q6)
 test = pg_curs.fetchall()
 for people in test:
-    print(f'Pclass: {people[0]}, Average Fare: ${round(people[1],2)}.')
+    print(f'Pclass: {people[0]}, Average Fare: ${round(people[1], 2)}.')
 
 print('Question 6.5 By survival?')
 Q6_5 = """
@@ -163,7 +173,7 @@ From Titanic as c GROUP by  Pclass, Survived;"""
 pg_curs.execute(Q6)
 test = pg_curs.fetchall()
 for people in test:
-    print(f'Pclass: {people[0]}, Average Fare: ${round(people[1],2)}.')
+    print(f'Pclass: {people[0]}, Average Fare: ${round(people[1], 2)}.')
 
 print('Question 7  How many siblings/spouses',
       'aboard on average, by passenger class?')
@@ -176,7 +186,7 @@ test = pg_curs.fetchall()
 for people in test:
     print(
         f'Pcalss: {people[0]}, Average number of Sibling/ Spouse:'
-        f'{round(people[1],2)}.')
+        f'{round(people[1], 2)}.')
 
 print("Question 7.5 By survival?")
 Q7_5 = '''
@@ -187,7 +197,7 @@ test = pg_curs.fetchall()
 for people in test:
     print(
         f'Survived: {people[0]}, Average number of Sibling/ Spouse:'
-        f' {round(people[1],2)}.')
+        f' {round(people[1], 2)}.')
 
 print("Question 8 How many parents/children ",
       "aboard on average, by passenger class? By survival?")
@@ -200,7 +210,7 @@ test = pg_curs.fetchall()
 for people in test:
     print(
         f'Pclass: {people[0]}, ',
-        f'Average number of Parent/Chidren: {round(people[1],2)}.')
+        f'Average number of Parent/Chidren: {round(people[1], 2)}.')
 
 print("Question 8.5 By survival?")
 Q8_5 = '''
@@ -211,4 +221,4 @@ test = pg_curs.fetchall()
 for people in test:
     print(
         f'Pclass: {people[0]},',
-        f'Average number of Parent/Chidren: {round(people[1],2)}.')
+        f'Average number of Parent/Chidren: {round(people[1], 2)}.')
